fcfd176825902e91d213d73c5ae3941de52927e7
Your Name <graceliu@gracelius-iMac.home>
2022-02-23 16:58:45 -0500

    commit

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -26,12 +26,27 @@ type Reaction{
     createdAt: String
     username: String
 }
+type Auth {
+    token: ID!
+    user: User
+}
 type Query {
+    me:User
     users:[User]
     user(username:String!):User
     thoughts(username:String):[Thought]
     thought(_id:ID!):Thought
 }
+
+type Mutation{
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId:ID!, reactionBody: String!):Thought
+    addFriend(friendId:ID!): User
+    
+}
+
 `;
 
 //export the typeDefs
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const {ApolloServer}=require('apollo-server-express')
 //import our typeDefs and resolvers
 const {typeDefs, resolvers} =require('./schemas')
 const db = require('./config/connection');
+const { authMiddleware } = require('./utils/auth');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -14,7 +15,7 @@ const startServer =async()=>{
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    //context:authMiddleware
+    context:authMiddleware
   });
 
   // Start the Apollo server
