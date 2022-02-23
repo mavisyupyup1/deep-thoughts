2cd9ef6ca9e51a6aa16d5bc2c4aca117d7af0aab
mavisyupyup1 <mavisyupyup1@github.com>
2022-02-23 11:10:02 -0500

    set up query

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,24 @@
+const {User, Thought}=require('../models')
 const resolvers ={
     Query:{
-        helloWorld:()=>{
-            return 'Hello World!';
+        thoughts:async(parent,{username})=>{
+            const params = username ?{username}:{};
+            return Thought.find(params).sort({createdAt:-1})
+        },
+        thought:async(parent,{_id})=>{
+            return Thought.findOne({_id})
+        },
+        users:async()=>{
+            return User.find()
+            .select('-__v -password')
+            .populate('friend')
+            .populate('thoughts')
+        },
+        user: async (parent,{username})=>{
+            return User.findOne({username})
+            .select('-__v -password')
+            .populate('friends')
+            .populate('thoughts')
         }
     }
 }
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,10 +2,37 @@
 const {gql} = require ('apollo-server-express')
 
 //create out typeDefs
-const typeDefs = gql`
-type Query{
-    helloWorld: String
-}`;
+const typeDefs =gql`
+type User {
+    _id:ID
+    username:String
+    email: String
+    friendCount: Int
+    thoughts:[Thought]
+    friends:[User]
+}
+type Thought {
+_id:ID
+thoughtText: String
+createdAt: String
+username: String
+reactionCount: Int
+reactions:[Reaction]
+}
+
+type Reaction{
+    _id:ID
+    reactionBody: String
+    createdAt: String
+    username: String
+}
+type Query {
+    users:[User]
+    user(username:String!):User
+    thoughts(username:String):[Thought]
+    thought(_id:ID!):Thought
+}
+`;
 
 //export the typeDefs
 module.exports=typeDefs;
