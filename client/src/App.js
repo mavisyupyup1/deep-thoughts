b619983018d0ed8fd8ce57b90ce4bd4e215d46eb
mavisyupyup1 <mavisyupyup1@github.com>
2022-02-26 09:47:48 -0500

    add signle thought page and user profile page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import{BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
+import Login from './pages/Login'
+import NoMatch from './pages/NoMatch'
+import SingleThought from './pages/SingleThought'
+import Profile from './pages/Profile'
+import Signup from './pages/Signup'
+
 const httpLink = createHttpLink({
   uri:'/graphql',
 })
@@ -14,15 +21,26 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
+      <Router>
     <div className='flex-column justify-flex-start min-100-vh'>
       <Header />
       <div className='container'>
-        <Home />
+        <Switch>
+       <Route exact path ="/" component ={Home}></Route>
+       <Route exact path="/login" component={Login}/>
+      <Route exact path='/signup' component={Signup}/>
+      <Route exact path='/profile/:username?' component={Profile}/>
+      <Route exact path='/thoughts/:id' component={SingleThought}/>
+      <Route component={NoMatch}/>
+      </Switch>
       </div>
       <Footer />
     </div>
+    </Router>
     </ApolloProvider>
   );
 }
+
+
 
 export default App;
---
+++ client/src/components/FriendList/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const FriendList = ({friendCount,username, friends})=>{
+    if(!friends || !friends.length){
+        return <p className="bg-dark text-light p-3"> {username},make some friends!</p>
+    }
+
+    return(
+        <div>
+            <h5>
+                {username}'s {friendCount} {friendCount===1?'friend':'friend'}
+            </h5>
+            {friends.map(friend=>(
+                <button className ="btn w-100 display-black mb-2" key={friend._id}>
+                    <Link to={`/profile/${friend.username}`}>{friend.username}</Link>
+                </button>
+            ))}
+        </div>
+    )
+};
+export default FriendList
