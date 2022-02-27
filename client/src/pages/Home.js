73f8a432de46d378a82a11a48cd3788adc5fc6fc
mavisyupyup1 <mavisyupyup1@github.com>
2022-02-27 09:12:39 -0500

    home page updated

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ThoughtList from '../components/ThoughtList';
-import FriendList from '../components/FriendList';
 import ThoughtForm from '../components/ThoughtForm';
+import FriendList from '../components/FriendList';
+
 import Auth from '../utils/auth';
 import { useQuery } from '@apollo/client';
 import { QUERY_THOUGHTS, QUERY_ME_BASIC } from '../utils/queries';
@@ -18,7 +19,7 @@ const Home = () => {
       <div className="flex-row justify-space-between">
         {loggedIn && (
           <div className="col-12 mb-3">
-            <ThoughtForm/>
+            <ThoughtForm />
           </div>
         )}
         <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
