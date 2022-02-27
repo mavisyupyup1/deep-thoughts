d60e42fcdb55f9aae3b7eb881f13fb0db0e70949
mavisyupyup1 <mavisyupyup1@github.com>
2022-02-26 19:42:54 -0500

    add friend button and thougth form

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -3,19 +3,30 @@ import { Redirect, useParams } from 'react-router-dom';
 
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
-
-import { useQuery } from '@apollo/client';
+import ThoughtForm from '../components/ThoughtForm';
+import { useQuery , useMutation} from '@apollo/client';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import { ADD_FRIEND } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const Profile = (props) => {
   const { username: userParam } = useParams();
-
+  const [addFriend] =useMutation(ADD_FRIEND)
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
     variables: { username: userParam },
   });
 
   const user = data?.me || data?.user || {};
+
+  const handleClick =async()=>{
+    try{
+      await addFriend({
+        variables: {id: user._id}
+      })
+    } catch(e){
+      console.error(e)
+    }
+  }
 
   // redirect to personal profile page if username is yours
   if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
@@ -35,6 +46,7 @@ const Profile = (props) => {
     );
   }
 
+
   return (
     <div>
       <div className="flex-row mb-3">
@@ -42,6 +54,12 @@ const Profile = (props) => {
           Viewing {userParam ? `${user.username}'s` : 'your'} profile.
         </h2>
       </div>
+      {userParam &&( 
+        <button className="btn ml-auto" onClick={handleClick}>
+        Add Friend 
+      </button>
+      )}
+    
 
       <div className="flex-row justify-space-between mb-3">
         <div className="col-12 mb-3 col-lg-8">
@@ -58,6 +76,9 @@ const Profile = (props) => {
             friends={user.friends}
           />
         </div>
+      </div>
+      <div className="mb-3">
+          {!userParam&&<ThoughtForm/>}
       </div>
     </div>
   );
